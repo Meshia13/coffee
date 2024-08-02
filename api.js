6ab65cf306c74920ce23abebcff1165df26372de
Carmeshia <65563803+Meshia13@users.noreply.github.com>
2024-08-02 19:31:23 -0400

    update: made changes to css, deleted app.j, created api.js

---
+++ api.js
@@ -0,0 +1,72 @@
+const coffeeContainer = document.querySelector('#coffee-container');
+
+function hotCoffee() {
+
+    fetch('https://api.sampleapis.com/coffee/hot')
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        displayHTML(data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
+  
+}
+
+function icedCoffee() {
+
+    fetch('https://api.sampleapis.com/coffee/iced')
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        displayHTML(data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
+  
+}
+
+
+function displayHTML(stuff) {
+
+    const divEl = document.createElement('div');
+    divEl.classList.add("coffee-list")
+    divEl.innerHTML = `
+     
+    <div class="title" id="title">
+        <h2>${stuff[2].title}</h2>
+    </div>
+
+    <figure class="coffee-image" id="coffee-image">
+        <img src=${stuff[2].image} alt="">
+    </figure>
+    
+    <div class="description" id="description">
+        ${stuff[2].description}
+    </div>
+
+    <h3>Ingredients:</h3>
+
+    <ul id="ingredientList">
+        <li>${stuff[2].ingredients}</li>
+    </ul>
+`
+    coffeeContainer.appendChild(divEl)
+
+}
+
+hotCoffee() 
+// icedCoffee()
+
--- app.js
+++ app.js
@@ -1,72 +1,162 @@
 
 
 
-const coffeeContainer = document.querySelectorAll('coffee-container');
+const coffeeContainer = document.querySelectorAll('#coffee-container');
 
-// Function to fetch the api
-async function getCoffee () {
 
-    try {
-        const response = await fetch('https://api.sampleapis.com/coffee/hot')
-    
-        if(!response.ok) {
-            throw new Error("Did not work")
+// function whatever() {
+
+//     fetch('https://api.sampleapis.com/coffee/hot')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log('Data received:', data);
+//   })
+//   .catch(error => {
+//     console.error('There was a problem with the fetch operation:', error);
+//   });
+
+  
+// }
+
+// whatever() 
+
+function coffeeMaker() {
+    fetch('https://api.sampleapis.com/coffee/hot')
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
         }
+        return response.json();
+    })
+    .then(data => {
+        getDisplay(data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
+}
+
+coffeeMaker()
+
+
+
+// async function getCoffee() {
+
+    
+//         const response = await fetch('https://api.sampleapis.com/coffee/hot')
+    
+//         if(!response.ok) {
+//             throw new Error("Did not work")
+//         }
 
         
-        const data = await response.json();
+//         const data = await response.json();
         
 
-        // data.slice(0,3)
-        displayCoffee(data);
-        // showCoffee(data)
-        // getDisplay(data);
-        console.log(data);
-    }
+//         getDisplay(data);
+//         // console.log(data);
+    
 
-    catch(error) {
-        console.log('Error fetching coffee data',error)
+  
+//         // getDisplay(res)
+    
+// }
 
-    }
+// getCoffee()
+
+function getDisplay(res) {
+    const coffeeContainer = document.querySelectorAll('#coffee-container');
+    
+    coffeeContainer.forEach(item => {
+        let results = `
+            <div class="title" id="title">
+                <h2>${item[0].title}</h2>
+            </div>
+        `;
+        return results
+        
+    })
+    
+    return res
 }
 
 
+// function coffeeMaker() {
 
-// function getDisplay(coffee) {
-//     const coffeeContainer = document.querySelectorAll('coffee-container');
-    
-//     coffeeContainer.forEach(item => {
-//         let results = `
-//             <div class="title" id="title">
-//                 <h2>${item.title}</h2>
-//             </div>
-//         `;
+//     fetch('https://api.sampleapis.com/coffee/hot')
+//     .then(response => response.json())
+//     .then(data => (console.log(data)))
 
+//     data
+
+// }
+
+// coffeeMaker()
+
+
+// function createCoffee(data) {
+
+//     // Parent element
+//     // const coffeeContainer = document.querySelectorAll('coffee-container');
+
+//     coffeeContainer.forEach(coffeeChoice => {
+//             // adding class name to element
+//         coffeeChoice.classList.add('coffee-card')
+//         // setting HTML content inside of 'coffee-card'
+//         coffeeChoice.innerHTML = `
         
+//             <div class="title" id="title">
+//                 <h2>${data.title}</h2>
+//             </div>
+
+//             <figure class="coffee-image" id="coffee-image">
+//                 <img src=${coffeeChoice.image} alt="">
+//             </figure>
+            
+//             <div class="description" id="description">
+//                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. In, facilis?
+//             </div>
+
+//             <h3>Ingredients:</h3>
+
+//             <ul id="ingredientList">
+//                 <li></li>
+//             </ul>
+
+//         `;
 //     })
+
     
     
 // }
 
-function showCoffee(data) {
+// createCoffee(coffeeMaker())
 
-    window.addEventListener('DOMContentLoaded', function() {
+// function showCoffee(data) {
+
+//     window.addEventListener('DOMContentLoaded', function() {
     
 
     
 
-    function displayData(results) {
-        results = `
-        <div class="title" id="title">
-            <h2>${data.title}</h2>
-        </div>
-        `
-        coffeeContainer.innerHTML = results
-    }
+//     function displayData(results) {
+//         results = `
+//         <div class="title" id="title">
+//             <h2>${data.title}</h2>
+//         </div>
+//         `
+//         coffeeContainer.innerHTML = results
+//     }
    
-    coffeeContainer.forEach(displayData)
-})
-}
+//     coffeeContainer.forEach(displayData)
+// })
+// }
 
 
 
@@ -76,44 +166,10 @@ function displayCoffee(data) {
     // window.addEventListener('DOMContentLoaded', function() {
    
 
-    function createCoffee(data) {
-
-        // Parent element
-        // const coffeeContainer = document.querySelectorAll('coffee-container');
-
-        coffeeContainer.forEach(coffeeChoice => {
-                // adding class name to element
-            coffeeChoice.classList.add('coffee-card')
-            // setting HTML content inside of 'coffee-card'
-            coffeeChoice.innerHTML = `
-            
-                <div class="title" id="title">
-                    <h2>${data.title}</h2>
-                </div>
-
-                <figure class="coffee-image" id="coffee-image">
-                    <img src=${coffeeChoice.image} alt="">
-                </figure>
-                
-                <div class="description" id="description">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. In, facilis?
-                </div>
-
-                <h3>Ingredients:</h3>
-
-                <ul id="ingredientList">
-                    <li></li>
-                </ul>
-
-            `;
-        })
-
-        
-        
-        }
+    
         coffeeContainer.forEach(createCoffee)
        
     // })
 }
 
-getCoffee()
+// getCoffee()
